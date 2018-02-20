145ead5b8ad420f6fa8379c5a84a71431ca5df38
logendhiran <36626445+logendhiran@users.noreply.github.com>
2018-02-20 17:26:24 +0530

    Create program6.cpp

---
+++ program6.cpp
@@ -0,0 +1,18 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+	char ch;
+	cout<<"Enter a character: ";
+	cin>>ch;
+	if((ch>='a'&& ch<='z') || (ch>='A' && ch<='Z'))
+	{
+		cout<<ch<<" alphabet";
+	}
+	else
+	{
+		cout<<ch<<" alphabet";
+	}
+	getch();
+}
