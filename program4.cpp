651b0274b74630b40627817abe76a45cacdc41c4
logendhiran <36626445+logendhiran@users.noreply.github.com>
2018-02-20 17:11:22 +0530

    Create program4.cpp

---
+++ program4.cpp
@@ -0,0 +1,15 @@
+int main()
+{
+    char ch;
+    printf("Enter an alphabet: \n");
+    scanf(" %c", &ch);
+    if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+		ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')  {
+		printf("\n %c is a VOWEL", ch);
+	}
+    else
+    {
+    	printf("\n %c is a CONSONANT", ch);
+      }
+    return 0;
+}
