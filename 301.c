a4fe05ebaf2713ea0e841eefed4dde16de5dcf22
logendhiran <36626445+logendhiran@users.noreply.github.com>
2018-02-23 17:57:01 +0530

    Create 301.c

---
+++ 301.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int  i,number,difference,firstterm,sum=0;
+printf("enter the numberofseries,firstterm,difference");
+scanf("%d\t%d\t%d",&number,&firstterm,&difference);
+for(i=0;i<number;i++)
+{
+sum=+number;
+firstterm=+difference;
+}printf("\n%d",sum);
+return 0;
+}
