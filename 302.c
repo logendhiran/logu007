185d8f9815b356d438c924d5150cce949d2b03c1
logendhiran <36626445+logendhiran@users.noreply.github.com>
2018-02-23 18:01:10 +0530

    Create 302.c

---
+++ 302.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+
+int  i,num,dif,first,sum=0;
+
+printf("enter the number of series,firstterm,difference");
+
+scanf("%d\t%d\t%d",&num,&first,&dif);
+
+for(i=0;i<num;i++)
+{
+
+sum=+num;
+first=+dif;
+
+}
+printf("\n%d",sum);
+return 0;
+}
