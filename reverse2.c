47411395c686846e290fce59addd03d8505a56fe
logendhiran <36626445+logendhiran@users.noreply.github.com>
2018-02-27 17:51:53 +0530

    Create reverse2.c

---
+++ reverse2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() 
+{
+    int a[10],i,n,rev=0,r;
+    scanf("%d",&n);
+    while(n1=0)
+    {
+        r=n%10;
+        rev=(rev*10)+r;
+        n=n/10;
+    }
+    printf("%d",rev);
+	return 0;
+}
