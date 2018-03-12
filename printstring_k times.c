3d424128c8897f6c1aa0a01e14d039338fed2792
logendhiran <36626445+logendhiran@users.noreply.github.com>
2018-03-12 17:50:47 +0530

    Create printstring_k times.c

---
+++ printstring_k times.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{   clrscr();
+
+    char s[80];
+    
+    int i,k;
+    
+    scanf("%s",s);
+    
+    scanf("%d",&k);
+    
+    for(i=0;i<k;i++)
+    
+    {
+        printf("%s\n",s);
+        
+    }
+    
+    return 0;
+}
