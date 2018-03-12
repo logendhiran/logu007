c0bf2a666c88c15dccb33cd8c5dfe67a14db0097
logendhiran <36626445+logendhiran@users.noreply.github.com>
2018-03-12 17:30:45 +0530

    Create bitwiseswap.c

---
+++ bitwiseswap.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<conio.h>
+int main(void)
+{
+  clrscr();
+  
+	int a,b,temp;
+  
+	scanf("%d %d",&a,&b);
+  
+	a=a^b;
+  
+	b=a^b;
+  
+	a=a^b;
+  
+	printf("%d %d",a,b);
+  
+	return 0;
+}
