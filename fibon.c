3c7701e32f4a367f2b9d185977534776d14b81c1
logendhiran <36626445+logendhiran@users.noreply.github.com>
2018-03-12 17:45:10 +0530

    Create fibon.c

---
+++ fibon.c
@@ -0,0 +1,26 @@
+#include<stdio.h> 
+
+int main()    
+
+{    
+ int a=0,b=1,c,i,number;    
+    
+ scanf("%d",&number);    
+ 
+ printf("\n%d %d",a,b);   
+ 
+ for(i=2;i<number;++i)
+ 
+ {    
+ 
+  c=a+b;    
+  
+  printf(" %d",c);    
+  
+  a=b;    
+  
+  b=c;    
+  
+ }  
+  return 0;  
+} 
