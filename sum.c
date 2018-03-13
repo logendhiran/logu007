7b93c26d640f112f2ac5304f4e15edabafb4b681
logendhiran <36626445+logendhiran@users.noreply.github.com>
2018-03-13 17:40:58 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <conio.h>
+int main(void) 
+{ 
+  clrscr();
+
+	int a,b,sum;
+  
+	printf("\n enter the first number:");
+  
+	scanf("%d",&a);
+  
+	printf("\n enter the second number:");
+  
+	scanf("%d",&b);
+  
+	sum=a+b;
+  
+	printf("%d",sum);
+  
+	return 0;
+}
