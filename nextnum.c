0bc9a49f2b842a0a88368319bc933484075e049d
logendhiran <36626445+logendhiran@users.noreply.github.com>
2018-03-13 17:19:01 +0530

    Create nextnum.c

---
+++ nextnum.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <conio.h>
+int main() 
+
+{
+    clrscr();
+    
+    int num;
+
+    scanf("%d",&num);
+
+    while(num<=100000)
+
+    {
+
+    num++;
+
+    break;
+
+    }
+
+    printf("%d",num);
+
+    return 0;
+}
