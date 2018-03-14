18aef686bd1f8caf36c8a3659503efeb55072eba
logendhiran <36626445+logendhiran@users.noreply.github.com>
2018-03-14 17:48:47 +0530

    Create swap5.c

---
+++ swap5.c
@@ -0,0 +1,35 @@
+
+#include <stdio.h>
+#include <conio.h>
+int main(void)  
+
+{
+clrscr();
+
+int x,y,temp;
+
+printf("\n Enter two numbers to swap:");
+
+scanf("%d %d",&x,&y);
+
+printf("\n Before swapping:");
+
+printf("\nx=%d",x);
+
+printf("\ny=%d",y);
+
+temp=x;
+
+x=y;
+
+y=temp;
+
+printf("\n After swapping:");
+
+printf("\nx=%d",x);
+
+printf("\ny=%d",y);
+
+return 0;
+
+}
