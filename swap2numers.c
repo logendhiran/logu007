45be0f8bd71316ad6b1656a8f4dbdfca064dd6b0
logendhiran <36626445+logendhiran@users.noreply.github.com>
2018-03-15 17:02:41 +0530

    Create swap2numers.c

---
+++ swap2numers.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+int main()
+{
+   int a,b,t;
+
+   scanf("%d %d",&a,&b);
+
+   if(a!=0)
+
+   {
+       t=a;
+       a=b;
+       b=t;
+   }
+
+   printf("%d %d",a,b);
+
+    return 0;
+}
