824c58237ba166a29a25fb801b0f5a88c64d74aa
logendhiran <36626445+logendhiran@users.noreply.github.com>
2018-03-15 17:50:39 +0530

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+#include <conio.h>
+int main() 
+
+{  
+    clrscr();
+    
+    int n,i,count=0;
+    
+    scanf("%d",&n);
+    
+    for(i=2;i<=(n/2);i++)
+    
+    {
+        if(n%i==0)
+         count++;
+    }
+    
+    if(count==0)
+    {
+        printf("yes");
+    } 
+    else
+    {
+        printf("no");
+    } 
+	return 0;
+}
