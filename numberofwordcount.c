acf410dcf374845e1b9f03e9511b56a5346d6430
logendhiran <36626445+logendhiran@users.noreply.github.com>
2018-03-15 17:33:06 +0530

    Create numberofwordcount.c

---
+++ numberofwordcount.c
@@ -0,0 +1,35 @@
+
+#include <stdio.h>
+#include <string.h>
+#include<conio.h>
+int main() 
+{
+  clrscr();
+  
+	char a[100];
+  
+	int n,i,count=0;
+  
+	gets(s);
+  
+	n=strlen(a);
+  
+	for(i=0;i<n;i++)
+  
+	{
+	    if(a[i]==' ')
+      
+	    {   
+	        count++;
+	    }
+      
+	    else
+      
+	    {
+	        continue;
+	    }
+	}
+  
+    printf("%d",count+1);
+	  return 0;
+}
