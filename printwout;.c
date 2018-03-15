2794b079ea659f3ea2925e3fd0d12d9ae899034d
logendhiran <36626445+logendhiran@users.noreply.github.com>
2018-03-15 17:42:20 +0530

    Create printwout;.c

---
+++ printwout;.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+int main(void) 
+{
+  clrscr();
+  
+	char s[300];
+  
+	int i; 
+  
+	scanf("%[^\t\n]s",s);
+  
+	if(printf("%s",s))
+  
+	{
+		
+	}
+  
+	return 0;
+}
