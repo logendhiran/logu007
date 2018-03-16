f01e7159c5afee8a2287bbfb208ffbd594fa4c49
logendhiran <36626445+logendhiran@users.noreply.github.com>
2018-03-16 17:01:43 +0530

    Create multiply7.c

---
+++ multiply7.c
@@ -0,0 +1,24 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{   
+    clrscr();
+    
+    int n;
+    
+    scanf("%d",&n);
+    
+    if(n%7==0)
+    
+    {
+        printf("yes..this number is multiple of 7");
+        
+    }
+    else
+    {
+    
+        printf("its not multiply of 7");   
+    }
+    
+return 0;
