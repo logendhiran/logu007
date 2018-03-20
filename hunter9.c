247580372159941f69dba01a8b6b6a269fcf4ca2
logendhiran <36626445+logendhiran@users.noreply.github.com>
2018-03-20 17:19:01 +0530

    Rename hunter8.c to hunter9.c

---
+++ hunter9.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    clrscr();
+   
+    int a[100],i,j,k=0,n=0,y=0,g=0,count=0;
+    
+    scanf("%d",&n);  
+    
+    for(i=0;i<n;i++)
+    
+    {
+        scanf("%d",&a[i]);
+        
+    }
+    for(i=0;i<n;i++)
+    
+    
+    {
+        k=a[i];
+        
+        for(j=0;j<n;j++)
+        
+        {
+        
+            y=a[j];
+            g=k+y;
+            if(g==0)
+            {
+            
+                printf("%d + %d =0",a[i],a[j]);
+                
+                
+                
+                printf("\n");
+                count++;
+            }
+            
+        }
+        
+        if(count==1)
+        {
+        
+            break;
+        }
+    }
+    
+return 0;
+}
