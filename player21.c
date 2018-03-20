6975d6213b2ba8982c8701c0be3510a92b841fdc
logendhiran <36626445+logendhiran@users.noreply.github.com>
2018-03-20 17:51:17 +0530

    Create player21.c

---
+++ player21.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+#include <conio.h>
+int main(void) 
+{
+  clrscr();
+  
+	char arr[500];
+  
+	scanf("%s",arr); 
+  
+	if(arr[0]=='s' || arr[0]=='S')
+  
+	{
+		printf("yes");
+    
+	}
+  
+	else
+  
+	{
+  
+		printf("no");
+	}
+  
+	return 0;
+}
