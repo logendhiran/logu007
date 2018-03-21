0cd88478bd9604f887bdbedeb533a2aae61238eb
logendhiran <36626445+logendhiran@users.noreply.github.com>
2018-03-21 17:26:38 +0530

    Create player24.c

---
+++ player24.c
@@ -0,0 +1,54 @@
+
+package info.gv;
+import java.util.*;
+public class Gv 
+{
+	public static void main(String[] args)
+	{
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt;
+		int temp = n;
+    
+		int c = 0,r,sum = 0;
+    
+		while(temp>) 
+		{
+			c++;
+      
+			temp /= 10;	
+		}
+    
+		temp = n;
+    
+		while(temp > 0)
+    
+    
+		{   
+			r = temp % 10;
+      
+			sum = sum + pow(r , c);
+			temp /= 10;
+		}
+    
+		System.out.println(sum);
+	}
+  
+	static int pow(int a, int b)
+  
+	{
+		int x,p=1;
+    
+    
+		for(x=1;x<=b;x++)
+    
+		{
+    
+			p = p*a;
+      
+		}
+    
+		return p;
+    
+	}
+  
+}
