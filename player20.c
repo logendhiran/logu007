dd6ad89c48db0f2b8b2885593bd281303d44552f
logendhiran <36626445+logendhiran@users.noreply.github.com>
2018-03-22 17:29:04 +0530

    Create player20.c

---
+++ player20.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include<string.h>
+#include<conio.h>
+int main(void)
+{
+  clrscr();
+  
+	char str1[60];
+  
+	int i,n;
+  
+	scanf("%s",str1);
+  
+	n=strlen(str1);
+  
+	for(i=0;i<n;i++)
+  
+	{
+		str1[i]=str1[i]+3;
+    
+		if(str1[i]=='X')
+    
+		str1[i]='v';
+    
+		if(str1[i]=='Y')
+    
+		str1[i]='u';
+    
+		if(str1[i]=='Z')
+    
+		str1[i]='w';
+    
+	}
+  
+	printf("%s",str1);
+
+
+	return 0;
+}
