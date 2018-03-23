a943cc646ddafbd6e4eef4ac79d3476bf9af34a1
logendhiran <36626445+logendhiran@users.noreply.github.com>
2018-03-23 17:43:20 +0530

    Create player37.c

---
+++ player37.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+
+{
+
+   char s[50];
+   
+   int i,l;
+   
+   scanf("%s",s);
+   
+   l=strlen(s);
+   
+   for(i=0;i<l;i++)
+   
+   {
+   
+     if(s[i]>='a' && s[i]<='z')
+     {
+     s[i]=s[i]-32;
+     }
+     
+     else
+     {
+     s[i]=s[i]+32;
+     }
+     printf("%c",s[i]);
+   }
+
+    return 0;
+}
