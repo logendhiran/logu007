16f30d84ce25765521dde5d01d891cb2d2637135
logendhiran <36626445+logendhiran@users.noreply.github.com>
2018-05-23 09:59:55 +0530

    Create beginner78.cpp

---
+++ beginner78.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int n;
+ cout<<"Enter the integer: ";
+ cin>>n;
+ {
+  if(n%13==0)  
+  {
+    cout<<"number is multiple of 13";
+    }
+    
+  else
+  {
+      cout<<"number is not multiple of 13";
+  }
+  }
+}
