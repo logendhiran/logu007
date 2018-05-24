b07bae28acf655551f44b8982900f837ad9a3ce9
logendhiran <36626445+logendhiran@users.noreply.github.com>
2018-05-24 15:54:24 +0530

    Create beginner91.c

---
+++ beginner91.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int celcius,kelvin;
+    printf("enter the temparature in celcius=");
+    
+    scanf("%d",&celcius);
+    
+    kelvin=celcius+273;
+    
+    printf("%d",kelvin);
+    
+    return 0;
+}
