89d067ea898a3b88a16ef332c97e13d6eb1364c1
logendhiran <36626445+logendhiran@users.noreply.github.com>
2018-05-24 10:06:22 +0530

    Create beginner82.cpp

---
+++ beginner82.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+
+{	
+
+   char a;
+   
+   int b;
+   
+   cout<<"Enter Any Character:";
+   
+   cin>>a;
+   
+   
+   b=(int)a;
+   
+   
+   printf("%d",b);
+   
+   
+	return 0;
+}
