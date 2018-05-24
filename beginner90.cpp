f7e1e755a6c4629555189b6058e1352b8fd8e1f2
logendhiran <36626445+logendhiran@users.noreply.github.com>
2018-05-24 11:04:55 +0530

    Create beginner90.cpp

---
+++ beginner90.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int i;
+	string s;
+	cin>>s;
+	for(i=0;s[i]!='\0';i++)
+	{
+		if(s[i]>='0' && s[i]<='9')
+		{
+			cout<<s[i];
+		}
+	}
+	return 0;
+  }
