2a3c9a65b27229468306214b12b31f39fb8b2ee8
logendhiran <36626445+logendhiran@users.noreply.github.com>
2018-05-25 11:52:09 +0530

    Create beginner101.c

---
+++ beginner101.c
@@ -0,0 +1,28 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    clrscr();
+
+    int a[10],min=0,i,n;
+    
+    min=a[0];
+    
+    for(i=0;i<10;i++)
+    
+    {
+    scanf("%d",&a[i]);
+    
+    }
+    for(i=1;i<10;i++)
+    
+    {
+    
+    if(a[i]<min)
+    
+    min=a[i];
+    }
+    printf("%d",min);
+return 0;
+}
