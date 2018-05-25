4e10380e96930665f0695e268714e00c46fc4a01
logendhiran <36626445+logendhiran@users.noreply.github.com>
2018-05-25 10:03:36 +0530

    Create beginnerkth.cpp

---
+++ beginnerkth.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+    int n,a[20],i,k,sum=0;
+    
+    cin>>n>>k;
+    
+    for(i=0;i<n;i++)
+    
+    {
+    
+    cin>>a[i];
+    
+    }
+    
+    cout<<a[k-1]<<endl;
+    
+    return 0;
+}
