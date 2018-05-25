c2a77b6d29f57fd120677f71b9b73fdda9017a63
logendhiran <36626445+logendhiran@users.noreply.github.com>
2018-05-25 12:25:31 +0530

    Create beginnerinttochar.cpp

---
+++ beginnerinttochar.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  clrscr();
+	int n;
+	printf("Enter the any  Number:");
+  
+	scanf("%d",&n);
+  
+	printf("The Integer Character is %c",(n+64));
+  
+	return 0;
+}
